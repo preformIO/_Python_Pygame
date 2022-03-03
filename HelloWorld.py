0e802a19809b4960774c372f2238054597b9a9c9
david allen <allend5@rpi.edu>
2022-03-03 08:05:12 -0500

    Classic "Hello World!" python app

---
+++ HelloWorld.py
@@ -0,0 +1,16 @@
+# imports
+
+
+# property declarations
+
+
+# helper functions
+
+
+# main function that is run at end of file to launch program
+def main ():
+  print("Hello World!")
+  
+  pass
+
+main()
