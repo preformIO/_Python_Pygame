0077d0060d5b40aafa393621dc3386b5fade9af7
david allen <allend5@rpi.edu>
2022-03-25 07:59:01 -0400

    Global render surface, counter, and keyboard input

--- HelloWorld_pygame.py
+++ HelloWorld_pygame.py
@@ -1,8 +1,8 @@
-#!/usr/bin/env python
 # -*- coding: utf-8 -*-
  
 # Módulos | imports
 import sys
+import keyboard
 import pygame
 from pygame.locals import *
 
@@ -10,6 +10,9 @@ from pygame.locals import *
 WIDTH = 640
 HEIGHT = 480
 
+win_sfc = None # initialize global for testing drawing on game surface
+render_i = None # gets incremented every draw loop
+ 
 # Clases | local classes
 # ---------------------------------------------------------------------
  
@@ -17,27 +20,65 @@ HEIGHT = 480
  
 # Funciones | local functions
 # ---------------------------------------------------------------------
- 
-# ---------------------------------------------------------------------
 
+# ---------------------------------------------------------------------
+ 
 # Funcion principal | main function definition
 # --------------------------------------------------------------------- 
 def main():
     # Crear una ventana | create window
-    screen = pygame.display.set_mode((WIDTH, HEIGHT))
+    pygame.display.set_mode((WIDTH, HEIGHT))
     pygame.display.set_caption("Pruebas Pygame | Pygame Tutorial")
-
-    # Mantener ventana abierta | keep window open
+    # get instance of window surface and store in global "win_sfc"
+    # to use later in drawing funcitons
+    global win_sfc
+    win_sfc = pygame.display.get_surface()
+    print(f'Got reference to surface "{win_sfc}"')
+    
+    # enter main draw / game loop
+    global render_i
+    render_i = 0
     while True:
+        # draw elements
+        # -------------
+        # clear background with dark grey
+        win_sfc.fill((15, 15, 15))
+        # draw other elements below this line
 
-        # Si queremos cerrar la ventana, deje de ejecutarse | exit on window quit
+        # update display (must be done every render loop)
+        # ----------
+        pygame.display.update()
+        pygame.time.delay(1000//30) # wait one 30th of a second betwen updates
+
+        # increment render count
+        # ----------
+        render_i = render_i + 1
+
+        # handle window quit event
+        # ----------
         for eventos in pygame.event.get():
             if eventos.type == QUIT:
                 sys.exit(0)
                 pass
             pass
         pass
-    
+
+        # handle keyboard input
+        # ----------
+        try:
+            # respond to "ENTER" presses
+            if keyboard.is_pressed('ENTER'):
+                print("\nyou pressed Enter..\n")
+                pass
+            # quit exit on "Esc" pressed
+            if keyboard.is_pressed('Esc'):
+                print("\nyou pressed Esc, so exiting...\n")
+                sys.exit(0)
+                pass
+            pass
+        except:
+            break
+
     return 0
     pass
 # ---------------------------------------------------------------------
--- README.md
+++ README.md
@@ -3,5 +3,7 @@ Python pygame Hello World app as example of python GUI or python game starter.
 
 ## References
 
-* [Pygame wiki (archived)](https://web.archive.org/web/20220119043005/http://www.pygame.org/wiki/tutorials)
+* [Pygame wiki](https://www.pygame.org/wiki/tutorials)
+    * [Pygame wiki (archived)](https://web.archive.org/web/20220119043005/http://www.pygame.org/wiki/tutorials)
 * [Razón Artificial (archived)](https://web.archive.org/web/20210117132645/http://razonartificial.com/2010/02/pygame-1-importar-inicializar/)
+* [Keyboard Input Basics](https://www.pygame.org/ftp/contrib/input.html)
